d929277643221d83c3001c56fb66ea0954fa4c3b
Mikhail <mehan@list.ru>
2022-09-07 16:35:50 +0300

    Task is done

--- Homework/Task 2/Program.cs
+++ Homework/Task 2/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = Prompt("Введите число: ");
+
+System.Console.WriteLine($"Таблицы кубов для числа {number}: ");
+for (int i = 1; i <= number; i++)
+{
+    System.Console.WriteLine(i*i*i);
+}
